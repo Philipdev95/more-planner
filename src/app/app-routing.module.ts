ee2ad886a9d0c848c6cabfaaf5b9ebdff89de4ca
Philipdev95 <philipdev95@gmail.com>
2024-08-04 16:04:16 +0200

    Startsida och inloggningssida och bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { GuestLoginComponent } from './guest-login/guest-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'employee-login', component: EmployeeLoginComponent },
+  { path: 'guest-login', component: GuestLoginComponent },
+  // Lägg till dina andra vägar här
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  constructor(private router: Router) { }
+
+  login(role: string) {
+    if (role === 'employee') {
+      // Logik för att logga in som anställd
+      this.router.navigate(['/employee-login']);
+    } else if (role === 'guest') {
+      // Logik för att logga in som gäst
+      this.router.navigate(['/guest-login']);
+    }
+  }
+}
